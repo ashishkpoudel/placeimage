4b1fdb564dc75cdf38db053afe960814956cbb96
Ashish K. Poudel <ashishp@khozinfo.com>
2019-03-07 09:26:59 +0545

    changes to angular and lumen app

--- client/src/app/services/contact.ts
+++ client/src/app/services/contact.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, of, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Contact } from 'src/app/models/contact';
 
@@ -9,11 +9,24 @@ import { Contact } from 'src/app/models/contact';
 export class ContactService {
 	constructor(private http_client: HttpClient){ }
 
-	post(contact: Contact): Observable <Contact> {
-		return this.http_client.post<Contact>('http://localhost:8000/contact', contact, {
+	post(contact: Contact): Observable<Contact> {
+		return this.http_client.post('http://localhost:8000/contact', contact, {
 			headers: new HttpHeaders({
 				'Content-Type': 'application/json'
 			})
-		});
+		}).pipe(
+			tap(_ => console.log('fetched contact')),
+			catchError((error) => this.handleError(error))
+		);
+	}
+
+	private handleError(errorResponse: HttpErrorResponse) {
+		if (errorResponse.error instanceof ErrorEvent) {
+			console.log('Client side error: ', errorResponse.error.message);
+		} else {
+			console.log('Server side error: ', errorResponse);
+		}
+
+		return new throwError('There is a problem with the service');
 	}
 }
