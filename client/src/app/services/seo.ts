43282809d50a245743b81410ea594b1c44b387f3
Ashish K. Poudel <ashishp@khozinfo.com>
2019-03-16 01:16:36 +0545

    update meta tags

--- client/src/app/services/seo.ts
+++ client/src/app/services/seo.ts
@@ -4,6 +4,6 @@ import { Meta } from '@angular/platform-browser';
 @Injectable({providedIn: 'root'})
 export class SEOService {
 	constructor(private meta: Meta) { 
-		this.meta.addTag({name: 'description', content: 'easy to use image placeholder online'});
+		// this.meta.addTag({name: 'description', content: 'easy to use image placeholder online'});
 	}
 }
